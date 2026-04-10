b45a95cb07157fba876dba981408cf166d0bdaa9
sheamckenziebc <sheamckenziebc@gmail.com>
2026-04-10 07:39:10 -0700

    rework

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sheamckenziebc.github.io',
-  base: '/Franzsika_Pottery',
+  base: '/JoseKnives',
   trailingSlash: 'always',
   integrations: [
     tailwind({
